9521c7133d8057ef798f5116fbd28e54cd42fe67
salih-ozsoy <salihhozsoy@gmail.com>
2023-09-10 14:24:07 +0300

    PhotoListState fixed

--- app/src/main/java/com/example/punkproject/data/state/PhotoListState.kt
+++ app/src/main/java/com/example/punkproject/data/state/PhotoListState.kt
@@ -2,10 +2,10 @@ package com.example.punkproject.data.state
 
 import com.example.punkproject.data.model.ResponseItem
 
-sealed class PhotoListState{
-    object Idle:PhotoListState()
-    object Loading:PhotoListState()
-    object Empty:PhotoListState()
-    class Result(val items:List<ResponseItem>):PhotoListState()
-    class Error(val throwable: Throwable):PhotoListState()
+sealed class PhotoListState {
+    object Idle : PhotoListState()
+    object Loading : PhotoListState()
+    object Empty : PhotoListState()
+    class Result(val items: List<ResponseItem>) : PhotoListState()
+    class Error(val throwable: Throwable? = null) : PhotoListState()
 }
